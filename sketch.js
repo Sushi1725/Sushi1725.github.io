2a5e0f2cc6cfc83ffacd4f666179666b79edeefb
Sushi1725 <86946104+Sushi1725@users.noreply.github.com>
2022-08-27 16:17:42 +1000

    Added border around video

--- sketch.js
+++ sketch.js
@@ -7,8 +7,9 @@ const serialPort = 'COM6';
 let classifier;
 let serial;
 let video;
-let flippedVideo;
 let label;
+let cameraBorder;
+let videoSize;
 
 function preload() {
     classifier = ml5.imageClassifier(modelURL + 'model.json');
@@ -17,27 +18,42 @@ function preload() {
 
 function setup() {
     serial.open(serialPort);
-    createCanvas(320, 260);
+    createCanvas(420, 420);
     video = createCapture(VIDEO);
+    cameraBorder = loadImage('camera_border.png');
+    videoSize = 260;
     video.size(320, 240);
+    poppinsBold = loadFont('Poppins-Bold.ttf');
     video.hide();
-    flippedVideo = ml5.flipImage(video);
+    // flippedVideo = ml5.flipImage(video);
     classifyVideo();
 }
 
 function draw() {
-    background(0);
-    image(flippedVideo, 0, 0);
-    fill(255);
-    textSize(16);
-    textAlign(CENTER);
-    text(label, width / 2, height - 4);
+
+    background(0, 0, 0);
+    noStroke();
+        textAlign(CENTER, CENTER);
+        textFont(poppinsBold);
+        textSize(14);
+        text("enable webcam access", width / 2, height / 1.6);
+        text("and refresh page to use", width / 2, height / 1.5);
+    image(video, width / 2 - videoSize / 2, height / 1.6 - videoSize / 2, videoSize, videoSize, 150, 0, videoSize * 1.5, videoSize * 1.5);
+    image(cameraBorder, width / 2 - videoSize / 2 - 3, height / 1.6 - videoSize / 2 - 3, videoSize + 6, videoSize + 6);
+
+    rectMode(CORNER);
+    classifyVideo();
+
+//     background('#E8F0FE');
+//     image(video, 0, 0);
+//     fill(0, 0, 0);
+//     textSize(16);
+//     textAlign(CENTER);
+//     text(label, width / 2, height - 4);
 }
 
 function classifyVideo() {
-    flippedVideo = ml5.flipImage(video)
-    classifier.classify(flippedVideo, gotResult);
-    flippedVideo.remove();
+    classifier.classify(video, gotResult);
 }
 
 function gotResult(error, results) {
